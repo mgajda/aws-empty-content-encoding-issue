04f793e865678e1aaf9cd41f1c7e1ef37e480537
Michal J. Gajda <mjgajda@migamake.com>
2019-08-09 15:38:56 +0200

    Enabled debugging.

--- app/Main.hs
+++ app/Main.hs
@@ -7,16 +7,18 @@ import Network.AWS.S3.PutObject
 import Control.Lens
 import Control.Monad.IO.Class
 import qualified Data.ByteString as BSL
+import           System.IO(stdout)
 
 bucketName = "test.migamake.com"
 
 main = do
-  env  <- newEnv Discover
+  lgr <- newLogger Debug stdout
+  env  <- newEnv Discover <&> set envLogger lgr
   runResourceT $ runAWS env $
     within Frankfurt $ do
       --content <- liftIO $ BSL.readFile "public/index.html" -- this works
       content <- chunkedFile 8192 "README.html" -- this results in empty metadata
       result  <- send $ set poContentType     (Just "text/markdown")
                -- $ set poContentEncoding (Just "identity") -- this breaks signing
-               $ putObject bucketName (ObjectKey "index.html") (toBody content)
+               $ putObject bucketName (ObjectKey "problem.html") (toBody content)
       liftIO   $ print result
