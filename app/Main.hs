3551ef7d3eca87e846432cfb5799082d66da6537
Michal J. Gajda <mjgajda@migamake.com>
2019-08-14 05:57:04 +0200

    Provides test URL.

--- app/Main.hs
+++ app/Main.hs
@@ -9,16 +9,16 @@ import Control.Monad.IO.Class
 import qualified Data.ByteString as BSL
 import           System.IO(stdout)
 
-bucketName = "test.migamake.com"
+bucketName = "migamake.com"
 
 main = do
   lgr <- newLogger Debug stdout
   env  <- newEnv Discover <&> set envLogger lgr
   runResourceT $ runAWS env $
-    within Frankfurt $ do
+    within Stockholm $ do
       --content <- liftIO $ BSL.readFile "public/index.html" -- this works
       content <- chunkedFile 65000 "cryptogame.svg" -- this results in empty metadata
       result  <- send $ set poContentType     (Just "image/svg+xml")
-               $ set poContentEncoding (Just "identity") -- this breaks signing
+               -- $ set poContentEncoding (Just "identity") -- this breaks signing
                $ putObject bucketName (ObjectKey "problem2.svg") (toBody content)
       liftIO   $ print result
